110092fa6674b3e1d884b52940b53772b7c0b0aa
Janne Parkkila <japskua@gmail.com>
2015-05-25 09:37:01 -0400

    Add users

---
+++ client/helpers/config.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Janne on 25.5.2015.
+ */
+Accounts.ui.config({
+    passwordSignupFields : 'USERNAME_ONLY'
+});
--- client/templates/application/layout.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Janne on 24.5.2015.
- */
-Template.layout.helpers({
-    pageTitle : function() {
-        return Session.get('pageTitle');
-    }
-});
