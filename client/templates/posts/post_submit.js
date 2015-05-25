358d73c2dbe4643475c51d10329a6cc0ca2aa4cf
Janne Parkkila <japskua@gmail.com>
2015-05-25 10:41:00 -0400

    Creating posts (chapter 7) finished

---
+++ client/templates/posts/post_submit.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Janne on 25.5.2015.
+ */
+Template.postSubmit.events({
+    'submit form' : function(e) {
+        e.preventDefault();
+
+        var post = {
+            url : $(e.target).find('[name=url]').val(),
+            title : $(e.target).find('[name=title]').val()
+        };
+
+        Meteor.call('postInsert', post, function(error, result) {
+            // Display the error to user and abort
+            if(error) {
+                return alert(error.reason);
+            }
+
+            // Show this result but route anyway
+            if (result.postExists) {
+                alert('This link has already been posted');
+            }
+            Router.go('postPage', { _id : result._id});
+        });
+
+    }
+});
