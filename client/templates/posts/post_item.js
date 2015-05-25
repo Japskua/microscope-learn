24e87a7b9b6e461f8f7039968197e1b4fb2650b4
Janne Parkkila <japskua@gmail.com>
2015-05-25 11:06:55 -0400

    Chapter 8 - Editing posts done

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -2,6 +2,9 @@
  * Created by Janne on 22.5.2015.
  */
 Template.postItem.helpers({
+    ownPost : function() {
+        return this.userId === Meteor.userId();
+    },
     domain : function() {
         var a = document.createElement('a');
         a.href = this.url;
